bdac5c5ab79ba0ec83ce4c80772de77b9057c93b
Flávio Santos <flavio.santos@pravaler.com.br>
2023-01-21 18:07:14 -0300

    feat: incluindo evento checkbox

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,48 +1,60 @@
-import styles from './Table.module.css'
+import { List } from './TableStyles';
+
 import { ClipboardText, Trash  } from 'phosphor-react';
+import { useEffect, useState } from 'react';
 
 export function Table() {
+  const [checkbox, setCheckebox] = useState(false)
+
+  const hasChecked = (e) => {
+    const result = e === false ? true : false
+    setCheckebox(result);
+  }
+
   return (
-    <div className={styles.table}>
-      <div className={styles.title}>
-        <span className={styles['title-left']}>
+    <List checkbox={checkbox}>
+      <div className="title">
+        <span className='title-left'>
           Tarefas Criadas
-          <div className={styles['title-cicle']}>
+          <div className='title-cicle'>
             <span>0</span>
           </div>
         </span>
-        <span className={styles['title-right']}>
+        <span className='title-right'>
           Concluidas
-          <div className={styles['title-cicle']}>
+          <div className='title-cicle'>
             <span>0</span>
           </div>
         </span>
       </div>
-      
-      <div className={styles['table-line']} />
-      
-      <div className={styles['table-body']}>
-        <div className={styles['table-card']}>
-          <div className={styles['table-checkbox']}>
+      <div className='table-line' />
+      <div className='table-body'>
+        <div className='table-card'>
+          <div className='table-checkbox'>
             <label>
-              <input type="checkbox" id="cardcheckbox"/>
-                <span className={styles.checkmark}></span>
+              <input
+                type="checkbox"
+                id="card_checkbox"
+                defaultChecked={checkbox}
+                onChange={() => hasChecked(checkbox)}
+              />
+                <span className='checkmark'></span>
             </label>
               <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-          </div>  
-          <div className={styles['table-button-delete']}> 
+          </div>
+          <div className='table-button-delete'>
             <Trash weight="bold"/>        
           </div>
         </div>
       </div>
       
-      <div className={styles['table-text']}>
-        <div className={styles['table-icon']}>
+      <div className='table-text'>
+        <div className='table-icon'>
           <ClipboardText size={56} weight="thin" />
         </div>
         <strong>Você ainda não tem tarefas cadastradas</strong>
         <p>Crie tarefas e organize seus itens a fazer</p>
       </div>
-    </div>
+    </List>
   )
 }
